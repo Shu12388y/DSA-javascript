d4d2454195309df9cee1e6910164ee60a1838efd
Shu12388y <spl.sp999@gmail.com>
2023-07-11 19:42:26 +0530

    files are added

--- array/array_insertion/index.js
+++ array/array_insertion/index.js
@@ -16,9 +16,22 @@ for(let i=data.length-1;i>=position;i--){
    
 }
 
+// we can also use in built function
 
-for(let element of data){
-    console.log(element)
-}
+data.splice(2,0,9)
+console.log(data)
+// output : [ 1, 2, 9, 3, 4, 5 ]
+
+
+//Now if we want to insert element in end of the array
+
+data[data.length-1]=newValue;
+console.log(data)
+
+// output : [ 1, 2, 3, 4, 9 ]
+
+// for(let element of data){
+//     console.log(element)
+// }
 
 // output: [1,2,9,3,4,5]
